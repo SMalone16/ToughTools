fca8d68880dbdd1f810bd356cf8b95f68af3e65d
Stephen Malone <malones@pcsdny.org>
2025-11-07 12:52:14 -0500

    Fix CropState import for legacy wheat

--- src/main/java/com/smalone/toughwoodtools/InstantWheatListener.java
+++ src/main/java/com/smalone/toughwoodtools/InstantWheatListener.java
@@ -1,13 +1,13 @@
 package com.smalone.toughwoodtools;
 
 import org.bukkit.Material;
+import org.bukkit.CropState;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.material.Crops;
-import org.bukkit.material.CropState;
 
 public class InstantWheatListener implements Listener {
 
